b53401d4e375507ceac6ad8a5f7333496b4db203
1304561623 <1304561623@qq.com>
2024-07-05 13:57:51 +0800

    first commit

---
+++ LESPS-master/model/GaborDNANet/model_GaborDNANet.py
@@ -0,0 +1,244 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.conv import _ConvNd
+from torch.nn.modules.utils import _pair
+
+
+
+class VGG_CBAM_Block(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.ca = ChannelAttention(out_channels)
+        self.sa = SpatialAttention()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.ca(out) * out
+        out = self.sa(out) * out
+        out = self.relu(out)
+        return out
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+class Res_CBAM_block(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1):
+        super(Res_CBAM_block, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace = True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size = 3, padding = 1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        if stride != 1 or out_channels != in_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size = 1, stride = stride),
+                nn.BatchNorm2d(out_channels))
+        else:
+            self.shortcut = None
+
+        self.ca = ChannelAttention(out_channels)
+        self.sa = SpatialAttention()
+
+    def forward(self, x):
+        residual = x
+        if self.shortcut is not None:
+            residual = self.shortcut(x)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.ca(out) * out
+        out = self.sa(out) * out
+        out += residual
+        out = self.relu(out)
+        return out
+
+class GaborConv2d(_ConvNd):
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding = None, dilation=1, groups=1, bias=False, padding_mode='zeros'):
+        kernel_size = _pair(kernel_size)
+        stride = _pair(stride)
+        
+        if padding is None:
+            # 自动计算填充以确保输入输出尺寸相同
+            padding = (kernel_size[0] - 1) // 2
+        padding = _pair(padding)
+        
+        dilation = _pair(dilation)
+
+        super(GaborConv2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, False,
+                                          _pair(0), groups, bias, padding_mode)
+        self.freq = nn.Parameter(
+            (3.14 / 2) * 1.41 ** (-torch.randint(0, 5, (out_channels, in_channels))).type(torch.Tensor))
+        self.theta = nn.Parameter((3.14 / 8) * torch.randint(0, 8, (out_channels, in_channels)).type(torch.Tensor))
+        self.psi = nn.Parameter(3.14 * torch.rand(out_channels, in_channels))
+        self.sigma = nn.Parameter(3.14 / self.freq)
+        self.x0 = torch.ceil(torch.Tensor([self.kernel_size[0] / 2]))[0]
+        self.y0 = torch.ceil(torch.Tensor([self.kernel_size[1] / 2]))[0]
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = device
+
+    def forward(self, input_image):
+        y, x = torch.meshgrid([torch.linspace(-self.x0 + 1, self.x0, self.kernel_size[0]),
+                               torch.linspace(-self.y0 + 1, self.y0, self.kernel_size[1])])
+        x = x.to(self.device)
+        y = y.to(self.device)
+        weight = torch.empty(self.weight.shape, requires_grad=False).to(self.device)
+        for i in range(self.out_channels):
+            for j in range(self.in_channels):
+                sigma = self.sigma[i, j].expand_as(y)
+                freq = self.freq[i, j].expand_as(y)
+                theta = self.theta[i, j].expand_as(y)
+                psi = self.psi[i, j].expand_as(y)
+
+                rotx = x * torch.cos(theta) + y * torch.sin(theta)
+                roty = -x * torch.sin(theta) + y * torch.cos(theta)
+
+                g = torch.zeros(y.shape)
+
+                g = torch.exp(-0.5 * ((rotx ** 2 + roty ** 2) / (sigma + 1e-3) ** 2))
+                g = g * torch.cos(freq * rotx + psi)
+                g = g / (2 * 3.14 * sigma ** 2)
+                weight[i, j] = g
+                self.weight.data[i, j] = g
+        return F.conv2d(input_image, weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+
+
+class GaborDNANet(nn.Module):
+    def __init__(self, num_classes=1,input_channels=1, block=Res_CBAM_block, num_blocks=[2, 2, 2, 2], nb_filter=[16, 32, 64, 128, 256], deep_supervision=True, mode='test'):
+        super(GaborDNANet, self).__init__()
+        self.mode = mode
+        self.relu = nn.ReLU(inplace = True)
+        self.deep_supervision = deep_supervision
+        self.pool  = nn.MaxPool2d(2, 2)
+        self.up    = nn.Upsample(scale_factor=2,   mode='bilinear', align_corners=True)
+        self.down  = nn.Upsample(scale_factor=0.5, mode='bilinear', align_corners=True)
+
+        self.up_4  = nn.Upsample(scale_factor=4,   mode='bilinear', align_corners=True)
+        self.up_8  = nn.Upsample(scale_factor=8,   mode='bilinear', align_corners=True)
+        self.up_16 = nn.Upsample(scale_factor=16,  mode='bilinear', align_corners=True)
+
+        self.g0=GaborConv2d(input_channels,input_channels,kernel_size=(11,11))
+
+        self.conv0_0 = self._make_layer(block, input_channels, nb_filter[0])
+        self.conv1_0 = self._make_layer(block, nb_filter[0],  nb_filter[1], num_blocks[0])
+        self.conv2_0 = self._make_layer(block, nb_filter[1],  nb_filter[2], num_blocks[1])
+        self.conv3_0 = self._make_layer(block, nb_filter[2],  nb_filter[3], num_blocks[2])
+        self.conv4_0 = self._make_layer(block, nb_filter[3],  nb_filter[4], num_blocks[3])
+
+        self.conv0_1 = self._make_layer(block, nb_filter[0] + nb_filter[1],  nb_filter[0])
+        self.conv1_1 = self._make_layer(block, nb_filter[1] + nb_filter[2] + nb_filter[0],  nb_filter[1], num_blocks[0])
+        self.conv2_1 = self._make_layer(block, nb_filter[2] + nb_filter[3] + nb_filter[1],  nb_filter[2], num_blocks[1])
+        self.conv3_1 = self._make_layer(block, nb_filter[3] + nb_filter[4] + nb_filter[2],  nb_filter[3], num_blocks[2])
+
+        self.conv0_2 = self._make_layer(block, nb_filter[0]*2 + nb_filter[1], nb_filter[0])
+        self.conv1_2 = self._make_layer(block, nb_filter[1]*2 + nb_filter[2]+ nb_filter[0], nb_filter[1], num_blocks[0])
+        self.conv2_2 = self._make_layer(block, nb_filter[2]*2 + nb_filter[3]+ nb_filter[1], nb_filter[2], num_blocks[1])
+
+        self.conv0_3 = self._make_layer(block, nb_filter[0]*3 + nb_filter[1], nb_filter[0])
+        self.conv1_3 = self._make_layer(block, nb_filter[1]*3 + nb_filter[2]+ nb_filter[0], nb_filter[1], num_blocks[0])
+
+        self.conv0_4 = self._make_layer(block, nb_filter[0]*4 + nb_filter[1], nb_filter[0])
+
+        self.conv0_4_final = self._make_layer(block, nb_filter[0]*5, nb_filter[0])
+
+        self.conv0_4_1x1 = nn.Conv2d(nb_filter[4], nb_filter[0], kernel_size=1, stride=1)
+        self.conv0_3_1x1 = nn.Conv2d(nb_filter[3], nb_filter[0], kernel_size=1, stride=1)
+        self.conv0_2_1x1 = nn.Conv2d(nb_filter[2], nb_filter[0], kernel_size=1, stride=1)
+        self.conv0_1_1x1 = nn.Conv2d(nb_filter[1], nb_filter[0], kernel_size=1, stride=1)
+
+        if self.deep_supervision:
+            self.final1 = nn.Conv2d (nb_filter[0], num_classes, kernel_size=1)
+            self.final2 = nn.Conv2d (nb_filter[0], num_classes, kernel_size=1)
+            self.final3 = nn.Conv2d (nb_filter[0], num_classes, kernel_size=1)
+            self.final4 = nn.Conv2d (nb_filter[0], num_classes, kernel_size=1)
+        else:
+            self.final  = nn.Conv2d (nb_filter[0], num_classes, kernel_size=1)
+
+    def _make_layer(self, block, input_channels,  output_channels, num_blocks=1):
+        layers = []
+        layers.append(block(input_channels, output_channels))
+        for i in range(num_blocks-1):
+            layers.append(block(output_channels, output_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, input):
+        x_g0=self.g0(input)
+
+        x0_0 = self.conv0_0(x_g0)
+        x1_0 = self.conv1_0(self.pool(x0_0))
+        x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0)], 1))
+
+        x2_0 = self.conv2_0(self.pool(x1_0))
+        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0),self.down(x0_1)], 1))
+        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1)], 1))
+
+        x3_0 = self.conv3_0(self.pool(x2_0))
+        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0),self.down(x1_1)], 1))
+        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1),self.down(x0_2)], 1))
+        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2)], 1))
+
+        x4_0 = self.conv4_0(self.pool(x3_0))
+        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0),self.down(x2_1)], 1))
+        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1),self.down(x1_2)], 1))
+        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2),self.down(x0_3)], 1))
+        x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.up(x1_3)], 1))
+
+        Final_x0_4 = self.conv0_4_final(
+            torch.cat([self.up_16(self.conv0_4_1x1(x4_0)),self.up_8(self.conv0_3_1x1(x3_1)),
+                       self.up_4 (self.conv0_2_1x1(x2_2)),self.up  (self.conv0_1_1x1(x1_3)), x0_4], 1))
+
+        if self.deep_supervision:
+            output1 = self.final1(x0_1).sigmoid()
+            output2 = self.final2(x0_2).sigmoid()
+            output3 = self.final3(x0_3).sigmoid()
+            output4 = self.final4(Final_x0_4).sigmoid()
+            if self.mode == 'train':
+                return [output1, output2, output3, output4]
+            else:
+                return output4
+        else:
+            output = self.final(Final_x0_4).sigmoid()
+            return output
+
+
---
+++ LESPS-master/model/__init__.py
@@ -0,0 +1,4 @@
+from model.ACM.model_ALCNet import ASKCResNetFPN as ALCNet
+from model.ACM.model_ACM import ASKCResUNet as ACM
+from model.DNANet.model_DNANet import DNANet as DNANet
+from model.GaborDNANet.model_GaborDNANet import GaborDNANet as GaborDNANet
